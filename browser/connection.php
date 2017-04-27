9576043e7344f7b568d521883051e8da9f5e6296
Marc Verhagen <marc@cs.brandeis.edu>
2017-04-27 19:00:02 -0400

    Generalized database access

--- browser/connection.php
+++ browser/connection.php
@@ -1,9 +1,22 @@
 <?php
 
-$conn_hostname = 'localhost';
-$conn_username = 'batcave1_corelex';
-$conn_password = trim(file_get_contents('password.txt'));
-$conn_database = 'batcave1_corelex';
+require('debugging.php');
+
+//$DEBUG = true;
+
+foreach (file('config.txt') as $line) 
+{
+  if ($line[0] == '#') { continue; }
+  $fields = explode('=', $line);
+  if (count($fields) == 2) {
+     $key = trim($fields[0]);
+     $value = trim($fields[1]);
+     if ($key == 'hostname') { $conn_hostname = $value; }
+     elseif ($key == 'username') { $conn_username = $value; }
+     elseif ($key == 'password') { $conn_password = $value; }
+     elseif ($key == 'database') { $conn_database = $value; }
+  }
+}
 
 function show_connection() {
   global $conn_hostname, $conn_username, $conn_password, $conn_database;
